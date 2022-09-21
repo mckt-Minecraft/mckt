a3e0764829cce66b6ba1f2554f2f0e390875e2b3
Josiah (Gaming32) Glosson <soujournme@gmail.com>
2022-09-21 15:22:06 -0500

    Improve rockets

--- src/main/kotlin/io/github/gaming32/mckt/items/FireworkRocketHandler.kt
+++ src/main/kotlin/io/github/gaming32/mckt/items/FireworkRocketHandler.kt
@@ -7,7 +7,6 @@ import kotlin.random.Random
 
 object FireworkRocketHandler : ItemEventHandler {
     const val DURATION = 2 // Hardcoded Duration 1 firework rockets for now
-    private const val BASE_SPEED = 0.002297
 
     override suspend fun use(event: ItemEventHandler.UseEvent): ItemEventHandler.Result {
         val lifetime = 10 * DURATION * Random.nextInt(6) + Random.nextInt(7)
@@ -15,16 +14,15 @@ object FireworkRocketHandler : ItemEventHandler {
             val client = event.client
             val server = event.server
             var velocity = Vector3d.ZERO
-            repeat(lifetime) {
-                if (client.data.isFallFlying) {
-                    val rotation = client.rotationVector
-                    velocity += Vector3d(
-                        rotation.x * 0.01 + (rotation.x * 1.5 - velocity.x) * 0.5,
-                        rotation.y * 0.01 + (rotation.y * 1.5 - velocity.y) * 0.5,
-                        rotation.z * 0.01 + (rotation.z * 1.5 - velocity.z) * 0.5
-                    )
-                    client.sendPacket(SetEntityVelocityPacket(client.entityId, velocity.x, velocity.y, velocity.z))
-                }
+            for (i in 1..lifetime) {
+                if (!client.data.isFallFlying) break
+                val rotation = client.rotationVector
+                velocity += Vector3d(
+                    rotation.x * 0.01 + (rotation.x * 1.5 - velocity.x) * 0.5,
+                    rotation.y * 0.01 + (rotation.y * 1.5 - velocity.y) * 0.5,
+                    rotation.z * 0.01 + (rotation.z * 1.5 - velocity.z) * 0.5
+                )
+                client.sendPacket(SetEntityVelocityPacket(client.entityId, velocity.x, velocity.y, velocity.z))
                 server.waitTicks()
             }
         }
